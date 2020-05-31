776683bcb6d59e463b4bffd2ac76243eed437f04
Matthew Lluis <mwlluis@hotmail.com>
2020-05-31 13:48:41 -0400

    added on other questions in index.html and hid them in styles.css; added the transition from question 2 to question 3 in app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
-    
-    
+        
     if ($('#code-yes').prop('checked')) {
+      
       $("#question-two").show();
+    
     } else {
+      
       $("question-two").hide();
+
+    }
+  });
+
+  $("form#form-two").submit(function(event) {
+    event.preventDefault();
+
+    if ($('#ordinary-dislike').prop('checked')) {
+      
+      $("#question-three").show();
+    
+    } else {
+      
+      $("#question-three").hide();
+    
     }
   });
 });
