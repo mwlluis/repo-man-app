b8f3726caff86ab865901f06fd0deeaf909bc94d
Matthew Lluis <mwlluis@hotmail.com>
2020-06-09 17:04:39 -0400

    completed ui logic for bud, but business and ui logic is a little blurred, need to generalize ui logic before specifying the business logic, but I've arrived at a functional starting point nonetheless

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Business logic for determining character
+
+
+// User Interface Logic
+
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
@@ -26,4 +31,35 @@ $(document).ready(function() {
     
     }
   });
+
+  $("form#form-three").submit(function(event) {
+    event.preventDefault();
+
+    if ($('#commission-agree').prop('checked')) {
+
+      $("#question-four").show();
+
+    } else {
+
+      $("question-four").hide();
+
+    }
+  });
+
+  $("form#form-four").submit(function(event) {
+    event.preventDefault();
+    
+    if ( $('#code-yes').prop('checked') && $('#ordinary-dislike').prop('checked') 
+    && $('#commission-agree').prop('checked') && $('#goes-down-here').prop('checked') ) {
+
+      // $(location).attr('href', './links/bud.html');
+      window.open('./links/bud.html', "_blank");
+
+    } else {
+      
+      console.log("it didn't work");
+    }
+    
+  });
+ 
 });
