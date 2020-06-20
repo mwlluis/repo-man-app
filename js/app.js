fff20106665b43564f1b78c2ff1fa85cfb64dd4b
Matthew Lluis <mwlluis@hotmail.com>
2020-06-20 11:32:17 -0400

    started to build forking for distinguishing between characters by expanding if/else if/else statements

--- js/app.js
+++ js/app.js
@@ -11,14 +11,28 @@ $(document).ready(function() {
       
       $("#question-two").show();
     
-    } else if ($('#cosmos').prop('checked')) {
+    } else if ($('#attention').prop('checked')) {
 
-      $("#question-miller-one").show();
+      $("#question-otto-one").show();
 
     } else {
       
       $("question-two").hide();
 
+    }
+  });
+
+  $("form#form-five").submit(function(event) {
+    event.preventDefault();
+
+    if ($('#tense-yes').prop('checked')) {
+      
+      $("#question-three").show();
+    
+    } else {
+      
+      $("#question-three").hide();
+    
     }
   });
 
@@ -43,6 +57,10 @@ $(document).ready(function() {
 
       $("#question-four").show();
 
+    } else if ($('#commission-disagree').prop('checked')) {
+
+      $("question-four").show();
+
     } else {
 
       $("question-four").hide();
@@ -58,6 +76,12 @@ $(document).ready(function() {
 
       // $(location).attr('href', './links/bud.html');
       window.open('./links/bud.html', "_blank");
+
+    } else if ( $('#attention').prop('checked') && $('#tense-yes').prop('checked') 
+    && $('#commission-agree').prop('checked') && $('#chevy-malibu').prop('checked') ) {
+
+      window.open('./links/otto.html', "_blank");   
+
 
     } else {
       
